cd4ad0e7626765950ea9b35f85150ce774ae7cc6
Adolfo <adolfo.zignago@esmartit.es>
2022-10-12 11:33:09 +0200

    fix: adding cms and hotspot tables

---
+++ src/api/cms-device/routes/cms-device.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cms-device router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::cms-device.cms-device');
---
+++ src/api/radius-activity/services/radius-activity.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * radius-activity service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::radius-activity.radius-activity');
